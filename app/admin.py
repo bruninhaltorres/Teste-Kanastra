e064f85b60c30fb285b4e2d7c9966e7971415460
Bruna Torres <blts@ic.ufal.br>
2024-05-03 18:06:17 -0300

    Instalação feita

--- app/admin.py
+++ app/admin.py
@@ -5,6 +5,6 @@ class Persons(admin.ModelAdmin):
     list_display = ('name', 'governmentId', 'email', 'debtAmount', 'debtDueDate')
     list_display_links = ('name', 'governmentId')
     search_fields = ('governmentId',)
-    list_per_page = 20
+    list_per_page = 20 
 
 admin.site.register(Person, Persons)
