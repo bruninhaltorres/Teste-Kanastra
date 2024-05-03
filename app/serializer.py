46ab92714b5cfa3fc15eb05b8d04d8ef3f951347
Bruna Torres <blts@ic.ufal.br>
2024-05-03 18:39:07 -0300

    Validações adicionadas

--- app/serializer.py
+++ app/serializer.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
 from app.models import Person
 
+from app.validators import *
+
 class PersonSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Person
         fields = ['name', 'governmentId', 'email', 'debtAmount', 'debtDueDate']
+
+    def validate(self, data):
+        if not cpf_valido(data['governmentId']):
+            raise serializers.ValidationError({'governmentId': "Número de CPF inválido"})
+        if not nome_valido(data['name']):
+            raise serializers.ValidationError({'name': "Não inclua números nesse campo"})
+
+        return data
